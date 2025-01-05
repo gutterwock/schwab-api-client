a17d2160b3edb6a55b76b1435d7caa12c513539b
tony feng <thefunkyphresh@gmail.com>
2025-01-05 15:44:28 -0500

    handle case where no token file is present

--- config/index.js
+++ config/index.js
@@ -1,12 +1,17 @@
 const { callbackUrl, clientId, clientSecret } = require("./config.json");
-const token = require("./token.json");
+let token
+try {
+	token = require("./token.json");
+} catch {
+	token = {};
+}
 
 const oauthUrl = "https://api.schwabapi.com/v1/oauth";
 
 module.exports = {
-  callbackUrl,
-  clientId,
-  clientSecret,
-  oauthUrl,
-  token
+	callbackUrl,
+	clientId,
+	clientSecret,
+	oauthUrl,
+	token
 };
