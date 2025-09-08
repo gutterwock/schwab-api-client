57a6495dd354ace0c265d7b0f2c6ff376940abf8
tony feng <thefunkyphresh@gmail.com>
2025-09-08 18:57:27 -0400

    refactor auth flow

---
+++ src/index.js
@@ -0,0 +1,75 @@
+const url = require("url");
+const {
+	createReadline,
+	loadConfigs,
+	loadToken,
+	requestCodeUrl,
+	requestToken,
+	saveToken
+} = require("./auth/authHelper");
+
+class SchwabApiClient {
+	constructor({ configFile, tokenFile }) {
+		this.configFile = configFile;
+		this.tokenFile = tokenFile;
+		this.tokenType;
+		this.accessToken;
+		this.refreshToken;
+		this.tokenExpiration;
+		this.refreshExpiration;
+	};
+
+	setToken(tokenData) {
+		this.tokenType = tokenData.tokenType;
+		this.accessToken = tokenData.accessToken;
+		this.refreshToken = tokenData.refreshToken;
+		this.tokenExpiration = tokenData.tokenExpiration;
+		this.refreshExpiration = tokenData.refreshExpiration;
+	};
+
+	async authenticate() {
+		let readline;
+		try {
+			readline = createReadline();
+			const { callbackUrl, clientId, clientSecret } = loadConfigs(this.configFile);
+			let { tokenType, tokenExpiration, refreshExpiration, accessToken, refreshToken } = loadToken(this.tokenFile);
+
+			if (accessToken) {
+				if (Date.now() < tokenExpiration) {
+					console.log("Access token is still valid.")
+					this.setToken({ tokenType, tokenExpiration, refreshExpiration, accessToken, refreshToken });
+					return;
+				} else if (Date.now() < refreshExpiration) {
+					console.log("Access token has expired. Refreshing.")
+					const { data: refreshData } = await requestToken({ clientId, clientSecret, grantType: "refresh_token", refreshToken });
+					saveToken({ data: { ...refreshData, refreshExpiration }, outputFile: this.tokenFile });
+					this.setToken({ ...refreshData, refreshExpiration });
+					return;
+				} else {
+					console.log("Refresh token has expired. Requesting a new one.");
+				}
+			}
+
+			const codeUrl = await requestCodeUrl({ callbackUrl, clientId });
+			console.log("Code url: ", codeUrl);
+			let authCode;
+			const authedUrl = await new Promise(resolve => {
+				readline.question("Enter authenticated url:", resolve);
+			});
+			const { query: { code } } = url.parse(authedUrl, true);
+			authCode = decodeURIComponent(code);
+			refreshExpiration = Date.now() + 1000 * 60 * 60 * 24 * 7; // 7 days
+			const { data: tokenData } = await requestToken({ clientId, clientSecret, authCode, callbackUrl, grantType: "authorization_code" });
+			saveToken({ data: { ...tokenData, refreshExpiration }, outputFile: this.tokenFile });
+			this.setToken({ ...tokenData, refreshExpiration });
+
+			return;
+		} catch (err) {
+			console.error(`Failed to auth: ${err.message}\n${err.stack}`);
+		} finally {
+			readline?.close();
+		};
+	}
+};
+
+module.exports = SchwabApiClient;
